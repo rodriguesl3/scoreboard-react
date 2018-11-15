88f0fd50ce7e50f26e404291a0fe387eae33e8db
Lucas Rodrigues <lucasfr@programmer.net>
2018-11-14 23:56:27 -0200

    adicionado contador.

---
+++ scoreboard-react/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Contador from './Contador';
+
+ export default class App extends React.Component{
+
+  render(){
+    return (
+      <div>
+        <Contador />
+      </div>
+    )
+  }
+}
