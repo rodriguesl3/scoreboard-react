060f29bdee91200fde7f6f1e60909b004ffc5ff3
Lucas Rodrigues <lucasfr@programmer.net>
2018-11-20 01:01:01 -0200

    interação com react router

--- scoreboard-react/src/components/ScoreBoard/Partida.js
+++ scoreboard-react/src/components/ScoreBoard/Partida.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 
 export default class Partida extends Component {
+    constructor(propos){
+        super(propos);
+        this.state = {
+            estadio: "Morumbi/SP",
+            data: "01/12/2018",
+            horario: "22hr",
+        }
+
+       
+    }
     render() {
         return (
             <div>
